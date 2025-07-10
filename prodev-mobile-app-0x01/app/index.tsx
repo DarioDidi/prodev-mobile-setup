dc0b37a014ac5c17bdc79c505f2da575f2afce5c
Dario Didi <dididario.dd2@gmail.com>
2025-07-10 23:16:37 +0300

    restructure view component app 0x01

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -5,12 +5,12 @@ export default function Index() {
   return (
     <>
       <View style={styles.container}>
-        <Text> Entry Screen - Awesome</Text>
-      </View>
-      <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
+        <Text>Entry Screen - Awesome</Text>
+        <View style={styles.container}>
+          <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+          <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+          <Text style={styles.smallText}>ALX is awesome</Text>
+        </View>
       </View>
     </>
   );
